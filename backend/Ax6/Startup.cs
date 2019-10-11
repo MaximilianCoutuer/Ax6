840795c0f07e1ca51d5dd08a9643da9f85ab92a7
Maximilian Coutuer <Maximilian.Coutuer@realdolmen.com>
2019-10-11 22:45:16 +0200

    Before remove Swagger project

--- backend/Ax6/Startup.cs
+++ backend/Ax6/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ax6.Domain.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Ax6
 {
@@ -26,11 +28,24 @@ namespace Ax6
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //services.AddTransient<Ax6Context>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AcADDemICTs EF", Version = "v1" });
+            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AX6");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

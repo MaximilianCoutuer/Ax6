879492e7ef0afa25013cf946df5bdb815f13dd77
Maximilian Coutuer <Maximilian.Coutuer@realdolmen.com>
2019-10-12 09:02:01 +0200

    Add ReviewController

---
+++ backend/Ax6/Controllers/ReviewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ax6.Domain;
+using Ax6.Domain.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ax6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private Ax6Context _context;
+
+        public ReviewController(Ax6Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("getreviewsforsubmission")]
+        public async Task<IActionResult> GetReviewsForSubmission(int id)
+        {
+            if (_context.Submissions.Where(x => x.Id == id).Count() == 0)
+            {
+                return BadRequest(id);
+            }
+            var reviews = _context.Reviews.Where(x => x.SubmissionId == id).ToList();
+            return Ok(reviews);
+        }
+
+        // GET: api/Review
+        [HttpGet]
+        [Route("criteria")]
+        public async Task<IActionResult> GetCriteria()
+        {
+            var criterias = _context.Criterias.ToList();
+            return Ok(criterias);
+        }
+
+        [HttpGet]
+        [Route("getreviewsforuser")]
+        public async Task<IActionResult> GetReviewsForUser(int id) 
+        {
+            if (_context.Users.Where(x => x.Id == id).Count() == 0)
+            {
+                return BadRequest(id);
+            }
+            var reviews = _context.Reviews.Where(x => x.SubmissionId == id).ToList();
+            return Ok(reviews);
+        }
+
+// GET: api/Review/5
+[HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Review
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Review/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

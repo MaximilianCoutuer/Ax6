5ad6d5fa4f8a03cdfc6c3dc1209738513cd5d08a
Maximilian Coutuer <Maximilian.Coutuer@realdolmen.com>
2019-10-12 08:34:40 +0200

    Seed db with default user + random merge stuff

--- backend/Ax6.Domain/Context/Ax6Context.cs
+++ backend/Ax6.Domain/Context/Ax6Context.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Ax6.Domain.Context
@@ -12,6 +13,15 @@ namespace Ax6.Domain.Context
         public DbSet<Criteria> Criterias { get; set; }
         public DbSet<Review> Reviews { get; set; }
         public DbSet<Criteria_Review> Criteria_Reviews { get; set; }
+
+        public Ax6Context()
+        {
+            if (Users.Count() == 0)
+            {
+                Users.Add(new User { Id = 1, Username = "admin", Password = "admin" });
+                SaveChanges();
+            }
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
